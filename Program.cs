b5b17b38c9c0e904f9745f4ec56a4c838ed098e8
Vugar Samadov <mi779pf7t@code.edu.az>
2023-10-18 23:23:27 +0400

    finished the task

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        int[] Array = {1,-2,3,-4,5,-6};
+        ConvertToPositive(ref Array);
+        
+        foreach (var a in Array)
+        {
+            Console.WriteLine(a);
+        }
+    }
+    
+    static void ConvertToPositive(ref int[] Array)
+    {
+        int[] PositiveArray = new int[Array.Length];
+        
+        for (int i = 0; i < Array.Length; i++)
+            PositiveArray[i] = Array[i] > 0 ? Array[i] : -Array[i];
+    
+        Array = PositiveArray;
+    }
+}
